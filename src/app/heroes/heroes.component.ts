8145d099cd8f463d0dcc7661546b175f5a4ef5d9
diprofe <Federica.DIPROPERZIO@netcompany-intrasoft.com>
2023-06-26 14:18:22 +0200

    log the history clicks

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/hero';
 import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   //heroes = HEROES;
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -22,6 +23,7 @@ export class HeroesComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     console.log(hero);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   /* getHeroes(): void {
